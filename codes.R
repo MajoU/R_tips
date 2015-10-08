415772677d83fe4ed279e01d65c7b157835bb23b
spindoc <johny.thintank@gmail.com>
2015-10-08 23:28:03 +0200

    new lines

--- codes.R
+++ codes.R
@@ -2,3 +2,8 @@ dt <- fread(some_csv)
 
 # removing every column that contains NA values
 df_NA_free <- dt[, colSums(is.na(df)) == 0]
+
+# how many 5 variables have each column in data frame?
+colSums(df == 5)
+
+
